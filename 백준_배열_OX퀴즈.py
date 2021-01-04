b4ce8e8bb5678d1bbb0450e244c78ac116f27426
haeinoh <76739472+haeinO@users.noreply.github.com>
2021-01-04 22:46:14 +0900

    Update 백준_배열_OX퀴즈.py

--- 백준_배열_OX퀴즈.py
+++ 백준_배열_OX퀴즈.py
@@ -2,7 +2,7 @@ case_num= int(input())
 for i in range(case_num):
     grade=0
     count=1
-    test_case=list(input())
+    test_case=input() #input의 default type은 str, str은 인덱싱이 가능하기 때문에 굳이 list로 형변환을 하지 않아도 됨
     quiz = len(test_case)
     for i in range(quiz):
         if test_case[i]=='O':
